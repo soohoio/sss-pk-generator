3bfc50fa1c1f9bee71714b9ac89f5e83bb8e77f9
dg.choi <dogeunchoi@gmail.com>
2022-05-26 17:34:31 +0900

    Modify systems

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,4 @@
-import { readFileSync, unlinkSync } from 'fs';
-import { resolve } from 'path';
-import {
-  generateKeyStore,
-  privateKeyToAddress,
-  unlockKeyStore,
-} from '../src/main';
+import { generateKey, privateKeyToAddress, unlockShares } from '../src/main';
 
 describe('sss-pk-generator', () => {
   let timeoutSpy: jest.SpyInstance;
@@ -22,97 +16,56 @@ describe('sss-pk-generator', () => {
     jest.runOnlyPendingTimers();
   });
 
-  it('Make a key store and secret shares', async () => {
-    // generate key store file in this directory
-    const res = generateKeyStore();
-    targetsToDelete.push(res);
+  const passphraseList = ['a', 'b', 'c', 'd', 'e'];
+  const threshold = 3;
+  let shares: {
+    message: string;
+    secret: string;
+    cipherparams: string;
+  }[] = [];
+  let newAddress = 'no_addr';
 
-    const filename = `keystore-${res.address}.json`;
+  it(`generateKey (N=${passphraseList.length}, T=${threshold})`, async () => {
+    const res = generateKey(passphraseList, threshold);
 
-    const keyJson: {
-      address: string;
-      ciphertext: string;
-    } = JSON.parse(
-      readFileSync(resolve(__dirname, '../', filename)).toString(),
-    );
+    expect(res.address.length).toBe(42);
+    expect(res.shares.length).toBe(passphraseList.length);
 
-    expect(keyJson.address.toLocaleLowerCase().indexOf('0x')).toBe(0);
-    expect(keyJson.address.toLocaleLowerCase().length).toBe(42);
-    expect(keyJson.ciphertext.length).toBeGreaterThan(0);
-
-    for (let i = 0; i < res.n; i++) {
-      const path = resolve(
-        __dirname,
-        '..',
-        `passphrase-shares-${i + 1}-${res.address}.json`,
-      );
-
-      const json: {
-        passphrase: string;
-      } = JSON.parse(readFileSync(path).toString());
-
-      expect(json.passphrase.length).toBeGreaterThan(0);
-    }
+    shares = res.shares;
+    newAddress = res.address;
   });
 
-  it('Could decrypt by the key store and secret shares', async () => {
-    const target = targetsToDelete[0];
-    const shares: string[] = [];
+  it('unlockShares', async () => {
+    {
+      const pk = unlockShares(shares);
 
-    for (let i = 0; i < target.n; i++) {
-      const path = resolve(
-        __dirname,
-        '..',
-        `passphrase-shares-${i + 1}-${target.address}.json`,
-      );
-
-      const json: {
-        passphrase: string;
-      } = JSON.parse(readFileSync(path).toString());
-
-      shares.push(json.passphrase);
+      expect(pk.length).toBe(64);
+      expect(newAddress).toBe(privateKeyToAddress(pk));
     }
-
-    expect(shares.length).toBe(target.n);
 
     {
-      const pk = unlockKeyStore(target.path);
-      expect(privateKeyToAddress(pk)).toBe(target.address);
+      const pk = unlockShares(shares.slice(0, threshold));
+
+      expect(pk.length).toBe(64);
+      expect(newAddress).toBe(privateKeyToAddress(pk));
     }
 
-    // {
-    //   const pk = unlockKeyStore(target.path, shares.slice(0, target.t));
-    //   expect(privateKeyToAddress(pk)).toBe(target.address);
-    // }
+    {
+      let errorHappned = false;
+      try {
+        const pk = unlockShares(shares.slice(0, threshold - 1));
 
-    // {
-    //   let pk: any = '';
-    //   try {
-    //     pk = unlockKeyStore(target.path);
-    //     privateKeyToAddress(pk);
-    //   } catch {
-    //     pk = 'ERR:catch';
-    //   }
-    //   expect(pk).toBe('ERR:catch');
-    // }
+        expect(pk.length).toBe(64);
+        expect(newAddress).toBe(privateKeyToAddress(pk));
+      } catch {
+        errorHappned = true;
+      }
+
+      expect(errorHappned).toBe(true);
+    }
   });
 
   afterAll(() => {
-    for (const target of targetsToDelete) {
-      const filename = `keystore-${target.address}.json`;
-      unlinkSync(resolve(__dirname, '../', filename));
-
-      for (let i = 0; i < target.n; i++) {
-        unlinkSync(
-          resolve(
-            __dirname,
-            '../',
-            `passphrase-shares-${i + 1}-${target.address}.json`,
-          ),
-        );
-      }
-    }
-
     timeoutSpy.mockRestore();
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
-import { resolve } from 'path';
-import { existsSync, readFileSync, writeFileSync } from 'fs';
 import * as CryptoJS from 'crypto-js';
-import { cwd } from 'process';
 
 const secrets = require('secrets.js-grempe');
 const _privateKeyToPublicKey = require('ethereum-private-key-to-public-key');
 const _publicKeyToAddress = require('ethereum-public-key-to-address');
-
-const _jsonKeyStringBuilder = (address: string, ciphertext: string) => {
-  return `{
-  "address": "${address}",
-  "ciphertext": "${ciphertext}"
-}`;
-};
-
-const _makeNodeJsonFile = (path: string, text: string) => {
-  writeFileSync(path, text);
-};
 
 const privateKeyToPublicKey = (privateKey: string) => {
   return _privateKeyToPublicKey(privateKey).toString('hex') as string;
@@ -28,12 +14,53 @@ const privateKeyToAddress = (privateKey: string) => {
   return publicKeyToAddress(privateKeyToPublicKey(privateKey));
 };
 
-const _encryptAES = (privateKey: string, secret: string): string => {
-  return CryptoJS.AES.encrypt(privateKey, secret).toString();
+const _aesJsonFormatter = {
+  stringify: (cipherParams: any) => {
+    const jsonObj: any = {
+      ct: cipherParams.ciphertext.toString(CryptoJS.enc.Hex),
+    };
+    if (cipherParams.iv) {
+      jsonObj.iv = cipherParams.iv.toString();
+    }
+    if (cipherParams.salt) {
+      jsonObj.s = cipherParams.salt.toString();
+    }
+    return JSON.stringify(jsonObj);
+  },
+  parse: (jsonStr: string) => {
+    const jsonObj = JSON.parse(jsonStr);
+    // extract ciphertext from json object, and create cipher params object
+    const cipherParams = CryptoJS.lib.CipherParams.create({
+      ciphertext: CryptoJS.enc.Hex.parse(jsonObj.ct),
+    });
+    // optionally extract iv or salt
+    if (jsonObj.iv) {
+      cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
+    }
+    if (jsonObj.s) {
+      cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
+    }
+    return cipherParams;
+  },
 };
 
-const _decryptAES = (ciphertext: string, secret: string): string => {
-  const bytes = CryptoJS.AES.decrypt(ciphertext, secret);
+const _encryptAES = (message: string, secret: string): string => {
+  const res = CryptoJS.AES.encrypt(message, secret, {
+    format: _aesJsonFormatter,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+
+  return res.toString();
+};
+
+const _decryptAES = (
+  cipherParams: string | { ct: string; iv: string; s: string },
+  secret: string,
+): string => {
+  const bytes = CryptoJS.AES.decrypt(cipherParams.toString(), secret, {
+    format: _aesJsonFormatter,
+  });
   return bytes.toString(CryptoJS.enc.Utf8);
 };
 
@@ -43,7 +70,9 @@ const split = (
   threshold: number,
 ): string[] => {
   const pwHex = secrets.str2hex(secretKey);
-  const shares = secrets.share(pwHex, number, threshold);
+  const shares = secrets
+    .share(pwHex, number, threshold)
+    .map((s) => s.toString());
 
   return shares;
 };
@@ -52,89 +81,43 @@ const combine = (shares: string[]): string => {
   return secrets.hex2str(comb);
 };
 
-const generateKey = (
-  passphraseLength: number,
-  number: number,
-  threshold: number,
-) => {
-  // make new random passphrase
-  const passphrase: string = secrets.random(passphraseLength * 4);
-  // divide passphrase into 3/5
+const generateKey = (passphraseList: string[], threshold: number) => {
+  const number = passphraseList.length;
+
+  if (number < threshold || threshold < 1) {
+    throw new Error('0 < threshold <= the number of passphrases');
+  }
+
   const newPrivateKey = secrets.random(256);
-
-  // START OF key store contents
-  const address = privateKeyToAddress(newPrivateKey);
-  const ciphertext = _encryptAES(newPrivateKey, passphrase);
-  // END OF key store contents
-
-  const shares = split(passphrase, number, threshold);
+  const shares = split(newPrivateKey, number, threshold);
 
   return {
-    address,
-    ciphertext,
-    shares,
+    address: privateKeyToAddress(newPrivateKey),
+    shares: shares.map((s, idx) => {
+      const res = _encryptAES(s, passphraseList[idx]);
+      return {
+        message: s,
+        secret: passphraseList[idx],
+        cipherparams: res,
+      };
+    }) as {
+      message: string;
+      secret: string;
+      cipherparams: string;
+    }[],
   };
 };
 
-const unlockShares = (ciphertext: string, shares: string[]) => {
-  const passphrase = combine(shares);
-  return _decryptAES(ciphertext, passphrase);
-};
+const unlockShares = (
+  pairs: {
+    cipherparams: string | { ct: string; iv: string; s: string };
+    secret: string;
+  }[],
+) => {
+  const shares = pairs.map((p) => _decryptAES(p.cipherparams, p.secret));
 
-const generateKeyStore = (dir = './') => {
-  const passphraseLength = 31;
-  const numberOfShares = 5;
-  const threshold = 3;
-
-  const res = generateKey(passphraseLength, numberOfShares, threshold);
-
-  const filename = `keystore-${res.address}.json`;
-  const path = resolve(cwd(), dir, filename);
-  const keystoreJson = _jsonKeyStringBuilder(res.address, res.ciphertext);
-
-  _makeNodeJsonFile(path, keystoreJson);
-
-  for (let i = 0; i < res.shares.length; i++) {
-    _makeNodeJsonFile(
-      resolve(cwd(), dir, `passphrase-shares-${i + 1}-${res.address}.json`),
-      `{
-  "passphrase": "${res.shares[i]}"
-}`,
-    );
-  }
-
-  return {
-    address: res.address,
-    n: numberOfShares,
-    t: threshold,
-    path,
-  };
-};
-
-const unlockKeyStore = (keystorePath: string) => {
-  const fileContent = readFileSync(keystorePath).toString();
-  const keyJson: {
-    address: string;
-    ciphertext: string;
-  } = JSON.parse(fileContent);
-
-  const shares: string[] = [];
-  for (let i = 0; i < 0xff; i++) {
-    const path = resolve(
-      keystorePath,
-      '../',
-      `passphrase-shares-${i + 1}-${keyJson.address}.json`,
-    );
-    const exists = existsSync(path);
-    if (exists) {
-      const json: {
-        passphrase: string;
-      } = JSON.parse(readFileSync(path).toString());
-      shares.push(json.passphrase);
-    }
-  }
-
-  return unlockShares(keyJson.ciphertext, shares);
+  const privateKey = combine(shares);
+  return privateKey;
 };
 
 export {
@@ -145,6 +128,4 @@ export {
   privateKeyToPublicKey,
   publicKeyToAddress,
   privateKeyToAddress,
-  generateKeyStore,
-  unlockKeyStore,
 };
